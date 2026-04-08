a75104c102873fa1ed5d5356930ea8135e6a0be6
Mae2604 <bondarenkom1@montclair.edu>
2026-04-08 18:46:41 -0400

    styling part 2
    front page, and role switcher

--- src/components/RoleSwitcher.jsx
+++ src/components/RoleSwitcher.jsx
@@ -1,6 +1,12 @@
 import {setRole} from '../app/actions'
 export default function RoleSwitcher({role,id}){
     return(
-        <button onClick={() => setRole(role,id)}>Switch role</button>
+        <><h1 className="text-4xl font-bold text-center mt-10 mb-3 ">Role Switcher page</h1>
+            <div className="rounded-lg ring-1 ring-gray-400 text-center bg-gray-100 dark:bg-gray-600 g-5 p-3 w-200 h-40 mx-140 shadow-lg mt-50">
+                <div className="space-y-2 text-center text-black-500 italic p-5">
+                    <p>Switch your role between deliverers and requesters!</p>
+                </div>
+                <button onClick={() => setRole(role,id)} className="w-52 h-10 bg-rose-600 hover:bg-red-700 text-white rounded-2xl block mx-auto mt-3">Switch Role</button>
+        </div></>
     )
 }
